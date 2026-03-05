f8d7fb8f5d1da1a7136608005812c57c16bf2ec0
TaianeAlbuquerqueDev <taiane.albuquerque.1994@gmail.com>
2026-03-05 09:54:23 -0300

    Criação das entidades

---
+++ src/main/java/br/com/senai/api_sistema_hospitalar/entity/Especialidade.java
@@ -0,0 +1,63 @@
+package br.com.senai.api_sistema_hospitalar.entity;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Especialidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "É necessário informar um nome!")
+    @Size(min = 3, max = 80, message = "Mínimo de 3 e máximo de 80 caracteres para o nome!")
+    private String nome;
+
+    @NotBlank(message = "É necessário inserir as informações médicas!")
+    @Size(min = 1, max = 1000, message = "Mínimo de 3 e máximo de 1000 caracteres para a descrição!")
+    private String descricao;
+
+    @OneToMany(mappedBy = "especialidade")
+    @JsonIgnore
+    private List<Medico> medicos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Medico> getMedicos() {
+        return medicos;
+    }
+
+    public void setMedicos(List<Medico> medicos) {
+        this.medicos = medicos;
+    }
+}
---
+++ src/main/java/br/com/senai/api_sistema_hospitalar/entity/Receita.java
@@ -0,0 +1,74 @@
+package br.com.senai.api_sistema_hospitalar.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+@Entity
+public class Receita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull(message = "É necessário informar a data de emissão!")
+    private LocalDateTime dataEmissao;
+
+    @NotBlank(message = "É necessário inserir as informações médicas!")
+    @Size(min = 1, max = 1000, message = "Mínimo de 3 e máximo de 1000 caracteres para a descrição de medicamentos!")
+    private String descricaoMedicamento;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_medico")
+    private Medico medico;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_paciente")
+    private Paciente paciente;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataEmissao() {
+        return dataEmissao;
+    }
+
+    public void setDataEmissao(LocalDateTime dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+
+    public String getDescricaoMedicamento() {
+        return descricaoMedicamento;
+    }
+
+    public void setDescricaoMedicamento(String descricaoMedicamento) {
+        this.descricaoMedicamento = descricaoMedicamento;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }  
+}
