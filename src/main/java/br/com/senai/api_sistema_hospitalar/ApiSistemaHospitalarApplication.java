a0662477290c51f114a3f78ea3a9b72de84f37a0
TaianeAlbuquerqueDev <taiane.albuquerque.1994@gmail.com>
2026-03-05 08:56:46 -0300

    Primeiro Commit - Criação do projeto

---
+++ src/main/java/br/com/senai/api_sistema_hospitalar/ApiSistemaHospitalarApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_sistema_hospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaHospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaHospitalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/api_sistema_hospitalar/ApiSistemaHospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_sistema_hospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaHospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
